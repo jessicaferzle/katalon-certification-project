59fd32e03ec7c80235194a1934ba2d64da6b53c5
jessicaferzle <jessica.ferzle@noema.net>
2025-05-12 14:34:45 +0300

    Add files via upload

---
+++ Test Suite 18.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 18</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>35b2be93-fb8a-44f0-9112-7e5773798955</testSuiteGuid>
+   <testCaseLink>
+      <guid>a5dc83df-6cc0-4c91-bccb-9a7fbc1f429c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/StudioAssist/New CMD 4</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
